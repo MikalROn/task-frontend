9657a23f39c2b77734c1dd71ae2736384e40832c
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2024-11-24 14:31:09 -0300

    fix: tela principal

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,26 +1,40 @@
 
-import { Component, EventEmitter, Input,  Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 import { Task } from '../model/task';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { TaskService } from '../services/task.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { CommonModule } from '@angular/common'; // Importação do CommonModule
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule]
+  imports: [MatTableModule, MatIconModule, MatButtonModule, CommonModule]
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit{
+    tasks: Task[] = [];
 
-    @Input() task: Task[] = [];
     @Output() add = new EventEmitter(false);
     @Output() edit = new EventEmitter(false);
     @Output() remove = new EventEmitter(false);
 
+    
     readonly displayedColumns = ['id', 'descricao', 'completo', 'actions'];
+
+    constructor(private taskService: TaskService) {}
+
+
+    ngOnInit(): void {
+      this.taskService.list().subscribe({
+        next: (data) => (this.tasks = data),
+        error: (err) => console.error('Erro ao carregar tarefas:', err),
+      });
+    }
 
     onAdd() {
       this.add.emit(true);
